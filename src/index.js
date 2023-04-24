714bda6d956b15cca81b10a1465ef71b0f2c1c9c
OHoltan <ola.holtan2303@gmail.com>
2023-04-24 10:41:45 +0200

    muligens ferdig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+import { 
+    getFirestore,
+    collection,
+    onSnapshot,
+    addDoc,
+    doc, 
+    query, 
+    orderBy 
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,35 +37,130 @@ const app = initializeApp(firebaseConfig);
 
 const input = document.querySelector('#input')
 const output = document.querySelector('#output')
+const namefield = document.querySelector(".namefield")
+const name = document.querySelector(".name")
+const addScore = document.querySelector(".add")
+const scoreboard = document.querySelector(".scorebord")
+
+
+
 
 const desimal = Math.random()*100
-const number = Math.round(desimal)
+let number = Math.round(desimal)
+const scoreboardleanght = 9
+
+let count = 0;
+let timerId;  
+
+
+
+// init services
+const db = getFirestore()
+
+// colection ref
+const colref = collection(db, 'ScoreBoard')
+
+// queries
+const q = query(colref, orderBy("score"))
+
+
+
 
 //console.log(number)
 
-document.addEventListener('keydown', e => {
+input.addEventListener('keydown', e => {
     if (e.key == "Enter") {
+        if (count == 0) {
+            startTimer()
+        }
+
         if (input.value == number) {
             //console.log("correct")
-            output.textContent = "Tallet er correct"
+            stopTimer()
+            output.textContent = "Tallet er Riktig"
+            namefield.classList.toggle("hidden")
+            
         } else if (input.value >= number) {
             //console.log("Number is smaller")
-            output.textContent = "Tallet er mindre"
+            output.textContent = "Tallet er Mindre"
+            addTen()
         } else {
             //console.log("number is bigger")
             output.textContent = "Tallet er Større"
+            addTen()
         }
         input.value = "";
         
     }
+    
 })
 
-function score(params) {
-    let count = 0;
 
-    setInterval(function() {
-        count + 
-        count++;
-        console.log(count);
-    }, 100);
+function incrementCount() {
+    count++;
+    document.getElementById("score").textContent = `score : ${count}`;
+    console.log(count)
+}
+
+function addTen() {
+    count += 10;
+}
+  
+function startTimer() {
+    timerId = setInterval(incrementCount, 100);
+}
+  
+function stopTimer() {
+    clearInterval(timerId);
+}
+
+
+
+addScore.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colref, {
+        score : count,
+        name : name.value
+    })
+    restart()
+})
+
+
+// Real time get collection data
+onSnapshot(q, (snapshot) => {
+    let playerScores = []
+    snapshot.docs.forEach((doc) => {
+        playerScores.push({...doc.data(), id: doc.id })
+        })
+        
+        const elementsToRemove = document.querySelectorAll('.scorelistli');
+
+
+        elementsToRemove.forEach(element => {
+            element.remove();
+        });
+
+        for (let i = 0; i < playerScores.length && i < scoreboardleanght; i++){ 
+            
+            const li = document.createElement('li')
+            li.textContent = `#${i+1} Name : ${playerScores[i].name}  Score : ${playerScores[i].score}`
+            li.classList.add("scorelistli")
+
+            scoreboard.append(li)
+
+        }   
+
+})
+
+
+function restart() {
+
+    
+    let randomnumber = Math.random()*100
+    number = Math.round(randomnumber)
+    count = 0
+    document.getElementById("score").textContent = `score : ${count}`;
+    namefield.classList.toggle("hidden")
+    
 }
